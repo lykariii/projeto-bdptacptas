3ed845698882f0568cfb32e73a67d7a50636d0a3
karen <karendecleva3@gmail.com>
2023-08-01 16:43:13 -0400

    Update page.js

--- Front-End/src/app/produto/[id]/page.js
+++ Front-End/src/app/produto/[id]/page.js
@@ -8,7 +8,7 @@ export default async function Produto({ params }) {
 
     const idJson = JSON.stringify(id);
 
-    const req = await fetch("http://localhost:3003/produtos", {
+    const req = await fetch("http://projeto-bdptacptas.vercel.app/produtos", {
         method: "POST",
         cache: "no-cache",
         headers: { 'content-type': 'application/json' },
